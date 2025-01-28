5fb6d34f0b4cee61a45f46999822a045f2a05c56
vishusaxena <vishusaxena3105@gmail.com>
2025-01-28 21:45:10 +0530

    make counter

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { useState } from "react"
 
 function App() {
-
+  let [count,setCount]=useState(0);
+  const addCount=()=>{
+    if(count<20)setCount(count+1)
+  }
+  const minusCount=()=>{
+    if(count>0)setCount(count-1)
+  }
   return (
-   <h1>vishu saxena</h1>
+    <>
+    <h1>Counter:{count}</h1>
+    <button onClick={addCount}>Increase</button><br/>
+    <button onClick={minusCount}>Decrease</button>
+
+
+    </>
+   
   )
 }
 
